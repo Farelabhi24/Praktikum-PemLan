47913bc4cd505c008c626466524544bdebb6cdb9
Farelabhi24 <49117249+Farelabhi24@users.noreply.github.com>
2025-03-13 13:01:07 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,25 @@
 
-// public class Main {
-//     public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
 
-//         BankAccount Account1 = new BankAccount("12345678", "Farel", 5000000);
-//         BankAccount Account2 = new BankAccount("89012344", "Abhi", 1000000);
+        BankAccount Account1 = new BankAccount("12345678", "Farel", 5000000);
+        BankAccount Account2 = new BankAccount("89012344", "Abhi", 1000000);
 
-//         System.out.println("Informasi Account 1:");
-//         Account1.displayInfo();
-//         System.out.println();
+        System.out.println("Informasi Account 1:");
+        Account1.displayInfo();
+        System.out.println();
         
-//         System.out.println("Informasi Account 2:");
-//         Account2.displayInfo();
-//         System.out.println();
+        System.out.println("Informasi Account 2:");
+        Account2.displayInfo();
+        System.out.println();
 
-//         System.out.println("Transaksi Account 1:");
-//         Account1.deposit(2000000);
-//         Account1.withdraw(500000);
-//         System.out.println();
+        System.out.println("Transaksi Account 1:");
+        Account1.deposit(2000000);
+        Account1.withdraw(500000);
+        System.out.println();
 
-//         System.out.println("Transaksi Account 2:");
-//         Account2.deposit(2000000);
-//         Account2.withdraw(1000000);
-//     }
-// }
+        System.out.println("Transaksi Account 2:");
+        Account2.deposit(2000000);
+        Account2.withdraw(1000000);
+    }
+}
